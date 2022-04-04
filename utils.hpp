ca7320d095dbe7d6c551f3405e385d1ddde2ccb9
xogml123 <xogml951@gmail.com>
2022-04-04 22:22:31 +0900

    vector

--- utils.hpp
+++ utils.hpp
@@ -180,6 +180,10 @@ namespace ft{
         }
         return (first2 != last2);
     };
+
+	template<class T> struct remove_const { typedef T type; };
+
+	template<class T> struct remove_const <const T> { typedef T type; };
 }
 
 
